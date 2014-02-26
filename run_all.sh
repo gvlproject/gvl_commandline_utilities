46f1442787785d4ffc31f99d8bca0cbb99f8a9f7
Clare Sloggett <claresloggett@gmail.com>
2014-02-26 23:27:00 +1100

    Added small master script to run (almost) everything in order with correct permissions.

---
+++ run_all.sh
@@ -0,0 +1,26 @@
+
+# Currently this will NOT add a non-sudo user (add_research_user.sh)
+# It will configure everything under the ubuntu account if run from there
+
+# Exit on any failure so we can troubleshoot
+set -e
+
+# Set up public_html ?
+
+# Create modules from Galaxy Toolshed tools
+# NB use sudo -E so that sudo keeps the MODULE environment variables
+echo "\n*** Creating environment modules for Galaxy Toolshed tools"
+sudo -E python toolshed_to_modules.py --force
+
+echo "\n*** Configuring ipython notebook server"
+
+# On the current image, jinja2 is not installed
+# This would be better bundled into the build config
+sudo pip install jinja2
+
+# Configure ipython notebook port forwarding and encryption
+# This would be better bundled into the build config
+sudo sh setup_nginx_forwarding.sh
+
+# Configure ipython notebook server
+python setup_ipython_server.py

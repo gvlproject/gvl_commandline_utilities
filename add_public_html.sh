3e17da0ed15430ae2d4eb1bb94655491ae83b70d
Clare Sloggett <claresloggett@gmail.com>
2014-03-23 23:07:04 +1100

    Adding script to create public_html redirect.

---
+++ add_public_html.sh
@@ -0,0 +1,40 @@
+
+# Add a public_html directory and redirect for the specified user
+# This script must be run as superuser
+#   e.g. usage:  sudo sh add_public_html.sh researcher
+
+# Clare Sloggett, VLSCI, University of Melbourne
+# Authored as part of the Genomics Virtual Laboratory project
+
+
+conf_file="/usr/nginx/conf/port_forwarding.conf"
+username=$1
+redirect="/public/"$username"/"
+
+if [ -z "$1" ]; then
+    echo "Require username on command line"
+    exit 1
+fi
+
+sudo su $username -c 'mkdir ~/public_html'
+sudo su $username -c 'chmod 755 ~/public_html'
+
+# Check if this user already exists in conf file
+
+if [ $(grep $redirect $conf_file | wc -l) != '0' ]; then
+    echo "User "$username" appears to already have a redirect in "$conf_file"; aborting."
+    exit 1
+fi
+
+# Add redirect
+
+cat >> $conf_file << END
+location $redirect {
+     alias /home/$username/public_html/;
+     expires 2h;
+     # Uncomment the following line to allow public browsing of public_html directory contents
+     # autoindex on;
+}
+END
+
+/usr/nginx/sbin/nginx -s reload

faf491f81a8737db82fdaffc8f97816f0a18bd09
Clare Sloggett <claresloggett@gmail.com>
2015-07-07 23:44:41 +0100

    Altered top-level scripts to run ansible playbook.

--- setup_user.sh
+++ setup_user.sh
@@ -1,109 +1,14 @@
 
-# Add a new (non-sudo) user and do per-user config of convenience utilities
-# Currently these are
-#  - symlink to genomes directory
-#  - symlink to galaxy app directory
-#  - public_html directory with nginx forwarding
-#  - configure an ipython notebook profile for secure access over the web
-#  - add script galaxy-fuse.py to use for direct access to Galaxy datasets
-#
-# Module files for toolshed tools are configured for all users, not here.
+# Add a new (non-sudo) user and do per-user config of convenience utilities.
+
+# This is now just a wrapper around ansible, provided for back-compatibility.
+
+# Note that if you want to set a separate password for the new user, you can
+# specify -e "use_ubuntu_password=no" on the ansible-playbook command line.
 
 # Clare Sloggett, VLSCI, University of Melbourne
 # Authored as part of the Genomics Virtual Laboratory project
 
-# Exit on any failure so we can troubleshoot
-set -e
-
-silent_mode=false
-
-# Parse command line arguments
-while getopts ":s" opt; do
-  case $opt in
-    s)
-       silent_mode=true
-      ;;
-  esac
-done
-shift $(($OPTIND-1))
-
 username=$1
 
-introduction="
-
-==== Creating new user account ====
-
-A new non-admin account is about to be configured with username '$username'.
-You will be asked for a password for this new account. This password will be used
-to log in as $username to linux, VNC, RStudio Server, and IPython Notebook.
-If you are concerned about security, you should give $username a different
-password to the ubuntu/CloudMan password.
-
-"
-
-echo "$introduction"
-
-# Create the user account
-echo "\n** Creating a non-sudo account for user "$username
-if [ $(getent passwd $username | wc -l) = '0' ]; then
-  # User does not exist yet
-  echo "Creating account for "$username
-  sudo adduser --disabled-password --gecos "" $username --home "/mnt/gvl/home/"$username
-  sudo ln -s "/mnt/gvl/home/"$username "/home/"$username
-else
-  echo "User "$username" already exists, not creating."
-fi
-
-echo "Setting password for "$username
-# get salted+hashed password for ubuntu user
-ubuntu_password=`sudo getent shadow | grep ubuntu: | awk -F":" '{ print $2 }'`
-if [ "$silent_mode" = true ] ; then
-   # replace new user's password with ubuntu user's password
-   echo $username:$ubuntu_password | sudo chpasswd -e
-else
-   sudo passwd $username
-fi
-
-echo "\n** Adding "$username" to appropriate groups"
-
-# Add user to rstudio_users, if that group exists
-if [ $(getent group rstudio_users | wc -l) != '0' ]; then
-    sudo usermod -a -G rstudio_users $username
-fi
-
-# Add user to fuse
-sudo usermod -a -G fuse $username
-
-# Set up public_html redirect for user at http://ip-addr/public/us.
-# This is dependent on an already existing
-# /etc/conf/public_html.conf (which may be empty)
-# configured into NGINX using configure_nginx.sh
-echo "\n** Creating a public_html directory for user "$username
-sudo sh add_public_html.sh $username
-
-# Add symlink to genomes directory on CloudMan instances
-echo "\n** Creating a symlink to Galaxy reference genomes for "$username
-sudo su $username -c 'if [ ! -d ~/galaxy_genomes ]; then ln -s /mnt/galaxyIndices/genomes ~/galaxy_genomes; fi'
-
-# Add symlink to galaxy app directory on CloudMan instances
-echo "\n** Creating a symlink to Galaxy for "$username
-sudo su $username -c 'if [ ! -d ~/galaxy ]; then ln -s /mnt/galaxy/galaxy-app ~/galaxy; fi'
-
-# Get galaxy-fuse
-echo "\n** Making copy of galaxy-fuse.py for "$username
-homedir=$(sudo su "$username" -c 'echo $HOME')
-sudo cp galaxy-fuse.py $homedir
-sudo chown $username":"$username $homedir"/galaxy-fuse.py"
-
-echo "\n** Configuring ipython notebook server for "$username
-
-# Configure ipython notebook server
-if [ "$silent_mode" = true ] ; then
-   sudo su $username -c 'python setup_ipython_server.py -s'
-else
-   sudo su $username -c 'python setup_ipython_server.py'
-fi
-
-# Write out user README file
-echo "\n** Writing ~/README.txt for "$username" - please consult for setup details.\n"
-sudo su $username -c 'python write_readme.py'
+sudo ansible-playbook playbook.yml --tags "setup_user" -e "new_user=$1"

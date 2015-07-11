01c41662f3d8cfbec806a1a1f32067ecc00d3df5
Clare Sloggett <claresloggett@gmail.com>
2015-07-11 13:08:02 +0100

    Upgrade IPython Notebook to 3

--- roles/gvl.commandline-utilities/templates/ipython_notebook_config.py
+++ roles/gvl.commandline-utilities/templates/ipython_notebook_config.py
@@ -13,10 +13,9 @@ c.NotebookApp.password = u'{{ password_python_hash }}'
 # Do not try any other ports.
 # If you are editing your config to allow multiple instances of IPython Notebook
 # to run simultaneously, you may want to change these settings.
+c.NotebookApp.ip = '127.0.0.1'
 c.NotebookApp.port = {{ ipython_port }}
 c.NotebookApp.port_retries = 0
 
 # Assume that we will run at a subdirectory when port-forwarded
-c.NotebookApp.base_project_url = '{{ ipython_location }}'
-c.NotebookApp.base_kernel_url = '{{ ipython_location }}'
-c.NotebookApp.webapp_settings = {'static_url_prefix':'{{ ipython_location }}static/'}
+c.NotebookApp.base_url = '{{ ipython_location }}'

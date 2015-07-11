bb94eec1f66180f6353078c7f6264543a850df3d
Clare Sloggett <claresloggett@gmail.com>
2015-07-11 09:22:48 +0100

    Added silent mode and better comments to wrapper scripts.

--- run_all.sh
+++ run_all.sh
@@ -5,7 +5,31 @@
 
 # This is now just a wrapper around ansible, provided for back-compatibility.
 
+# Usage: sh run_all.sh [-s]
+
+# Silent mode (-s) is equivalent to use_ubuntu_password=yes (the playbook default)
+# Non-silent mode is equivalent to use_ubuntu_password=no and will prompt for
+# a password.
+# Non-silent mode is the default in this wrapper script for back-compatibility.
+
 # Clare Sloggett, VLSCI, University of Melbourne
 # Authored as part of the Genomics Virtual Laboratory project
 
-sudo ansible-playbook playbook.yml
+silent_mode=false
+
+# Parse command line arguments
+while getopts ":s" opt; do
+  case $opt in
+    s)
+       silent_mode=true
+      ;;
+  esac
+done
+shift $(($OPTIND-1))
+
+# Call the master playbook
+if [ "$silent_mode" = true ] ; then
+    sudo ansible-playbook playbook.yml
+else
+    sudo ansible-playbook playbook.yml -e "use_ubuntu_password=no"
+fi
--- setup_user.sh
+++ setup_user.sh
@@ -3,6 +3,13 @@
 
 # This is now just a wrapper around ansible, provided for back-compatibility.
 
+# Usage: sh setup_user.sh [-s] username
+
+# Silent mode (-s) is equivalent to use_ubuntu_password=yes (the playbook default)
+# Non-silent mode is equivalent to use_ubuntu_password=no and will prompt for
+# a password.
+# Non-silent mode is the default in this wrapper script for back-compatibility.
+
 # Note that if you want to set a separate password for the new user, you can
 # specify -e "use_ubuntu_password=no" on the ansible-playbook command line.
 
@@ -11,4 +18,22 @@
 
 username=$1
 
-sudo ansible-playbook playbook.yml --tags "setup_user" -e "new_user=$1"
+silent_mode=false
+
+# Parse command line arguments
+while getopts ":s" opt; do
+  case $opt in
+    s)
+       silent_mode=true
+      ;;
+  esac
+done
+shift $(($OPTIND-1))
+
+username=$1
+
+if [ "$silent_mode" = true ] ; then
+    sudo ansible-playbook playbook.yml --tags "setup_user" -e "new_user=$1"
+else
+    sudo ansible-playbook playbook.yml --tags "setup_user" -e "new_user=$1" -e "use_ubuntu_password=no"
+fi

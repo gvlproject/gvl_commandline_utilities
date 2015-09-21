672a3a3ab9701e97367aa6cdd0913f02aeac1a0d
nuwan_ag <nuwan.ag@gmail.com>
2015-09-21 16:27:57 +1000

    Added default admin users + enabled logging.

--- roles/gvl.commandline-utilities/templates/jupyterhub_config.py
+++ roles/gvl.commandline-utilities/templates/jupyterhub_config.py
@@ -26,6 +26,8 @@ c.JupyterHub.base_url = '/jupyterhub'
 # The ip for this process
 c.JupyterHub.hub_ip = '127.0.0.1'
 
+# put the log file in /var/log
+c.JupyterHub.extra_log_file = '/var/log/jupyterhub.log'
 
 #------------------------------------------------------------------------------
 # Spawner configuration
@@ -33,3 +35,16 @@ c.JupyterHub.hub_ip = '127.0.0.1'
 
 # The IP address (or hostname) the single-user server should listen on
 c.Spawner.ip = '127.0.0.1'
+
+#------------------------------------------------------------------------------
+# Authenticator configuration
+#------------------------------------------------------------------------------
+
+# A class for authentication.
+#
+# The API is one method, `authenticate`, a tornado gen.coroutine.
+
+# set of usernames of admin users
+#
+# If unspecified, only the user that launches the server will be admin.
+c.Authenticator.admin_users = {'root', 'ubuntu'}
